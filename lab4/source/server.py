9e1f34267a31cd2d1bc45265f67ef116db829949
student <tomaszsztajkowski@gmail.com>
2022-11-24 12:41:24 +0100

    rest call

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -3,17 +3,15 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-def default():
-	return "Hello World!"
+@app.route("/num1=<num1>&num2=<num2>")
+def default(num1, num2):
+	numbers = [int(num1), int(num2)]
+	return {"sum" : numbers[0] + numbers[1],
+			"sub" : numbers[0] - numbers[1],
+			"mul" : numbers[0] * numbers[1],
+			"div" : numbers[0] / numbers[1],
+			"mod" : numbers[0] % numbers[1]}
 
-@app.route("/cmd/time/")
-def time():
-	return datetime.now().strftime("%H:%M:%S")
-	
-@app.route("/cmd/rev/<string>")
-def rev(string):
-	return string[::-1]
 
 if __name__ == "__main__":
 	app.run(debug=True)
