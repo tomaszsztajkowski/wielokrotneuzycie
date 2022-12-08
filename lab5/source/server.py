b9713c8522b40e8bfdad5f6c00b5ddf91386bb52
Tomasz Sztajkowski <tomaszsztajkowski@gmail.com>
2022-12-08 13:08:56 +0100

    changed host to access container from outside

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -4,17 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello, World!"
+    return "Hello, World!"
 
 @app.route("/num1=<num1>&num2=<num2>")
 def math(num1, num2):
-	numbers = [int(num1), int(num2)]
-	return {"sum" : numbers[0] + numbers[1],
-			"sub" : numbers[0] - numbers[1],
-			"mul" : numbers[0] * numbers[1],
-			"div" : numbers[0] / numbers[1],
-			"mod" : numbers[0] % numbers[1]}
+    numbers = [int(num1), int(num2)]
+    return {"sum" : numbers[0] + numbers[1],
+            "sub" : numbers[0] - numbers[1],
+            "mul" : numbers[0] * numbers[1],
+            "div" : numbers[0] / numbers[1],
+            "mod" : numbers[0] % numbers[1]}
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=4080)
+
