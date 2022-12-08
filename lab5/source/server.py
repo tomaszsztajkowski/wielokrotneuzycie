51ea781e350922b54136a5116549fec7b915e846
Tomasz Sztajkowski <tomaszsztajkowski@gmail.com>
2022-12-08 12:34:12 +0100

    cloned lab5

---
+++ lab5/source/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello, World!"
+
+@app.route("/num1=<num1>&num2=<num2>")
+def math(num1, num2):
+	numbers = [int(num1), int(num2)]
+	return {"sum" : numbers[0] + numbers[1],
+			"sub" : numbers[0] - numbers[1],
+			"mul" : numbers[0] * numbers[1],
+			"div" : numbers[0] / numbers[1],
+			"mod" : numbers[0] % numbers[1]}
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
